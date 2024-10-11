baf1ef5426963a8fc42b0c26d9ebe17cf7479872
Sheruq <greyo2006@gmail.com>
2024-10-11 18:16:24 +0300

    ирішено конфлікти і об'єднано історії

--- src/main/java/com/example/demo/Controllers/MainController.java
+++ src/main/java/com/example/demo/Controllers/MainController.java
@@ -6,19 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 
 @Controller
 public class MainController {
 
     @Autowired
-    private AriRepo ariRepo;
+    private AriRepo AriRepo;
 
     @GetMapping("/")
     public String home(Model model) {
-        Iterable<Arina> Arinass = ariRepo.findAll();
+        Iterable<Arina> Arinass = AriRepo.findAll();
         Arinass.forEach(arina -> System.out.println(arina.getTitle()));  // логування
         model.addAttribute("Arinass", Arinass);
         model.addAttribute("title", "Головна");
@@ -28,7 +32,7 @@ public class MainController {
 
     @GetMapping("/insert")
     public String insertadd(Model model) {
-        model.addAttribute("title","Додати книгу");
+        model.addAttribute("title", "Додати книгу");
         return "insert";
     }
 
@@ -36,7 +40,41 @@ public class MainController {
     @PostMapping("/insert")
     public String insertPostAdd(@RequestParam String title, @RequestParam String author, @RequestParam String status, @RequestParam int ISBN, @RequestParam int value, Model model) {
         Arina arina = new Arina(title, author, status, ISBN, value);
-        ariRepo.save(arina);
+        AriRepo.save(arina);
         return "redirect:/";
     }
+
+    @GetMapping("/{id}/edit")
+    public String BookEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!AriRepo.existsById(id)) {
+            return "redirect:/";
+        }
+        Optional<Arina> post = AriRepo.findById(id);
+        if (post.isPresent()) {
+            model.addAttribute("post", post.get()); // Передай сам об'єкт, а не список
+        }
+        return "edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String updateBook(@PathVariable(value = "id") long id,
+                             @RequestParam String title,
+                             @RequestParam String author,
+                             @RequestParam String status,
+                             @RequestParam int ISBN,
+                             @RequestParam int value) {
+        Optional<Arina> post = AriRepo.findById(id);
+        if (post.isPresent()) {
+            Arina arina = post.get();
+            arina.setTitle(title);
+            arina.setAuthor(author);
+            arina.setStatus(status);
+            arina.setISBN(ISBN);
+            arina.setValue(value);
+            AriRepo.save(arina);
+        }
+        return "redirect:/"; // Повертаємось на головну сторінку після редагування
+    }
+
+
 }
